b5ef97bb2ae90666731b794471ea4e28a24427cd
kemalma1 <mohammed.kemal@jhuapl.edu>
2022-11-12 01:27:43 -0500

    -Made changes so when login in, admin goes to admin page, guests go to guest page  -fixed bug related to issue 7

--- flask_app/models/workout.py
+++ flask_app/models/workout.py
@@ -6,9 +6,9 @@ from flask_bcrypt import Bcrypt
 from flask_app.models import user
 import re
 
-DB = "all_fitness_schema"
-
 class Workout:
+    db_name = "all_fitness_schema"
+
     def __init__(self, workout):
         self.id = workout["id"]
         self.experience = workout["experience"]
@@ -27,37 +27,27 @@ class Workout:
 
         query = """INSERT INTO workouts (experience ,goals ,days, routine, user_id) VALUES (%(experience)s, %(goals)s, %(days)s, %(routine)s, %(user_id)s);"""
 
-        workout = connectToMySQL(DB).query_db(query, workout_dict)
+        workout = connectToMySQL(cls.db_name).query_db(query, workout_dict)
         print("This is the workout id!!!!!", workout_dict)
-        # workout = cls.get_by_id(workout_id)
+        # workout = cls.get_workout_by_id(workout_id)
         # print(workout)
 
         return workout
 
-
-
-
-
-
-
-
     @classmethod
-    def get_by_id(cls, workout_dict):
+    def get_workout_by_id(cls, workout_dict):
         print(f"get workout by id{workout_dict}")
         data =  {"id" : workout_dict}
         query  =""" SELECT * FROM workouts
         JOIN users on users.id = workouts.user_id
         WHERE workouts.id = %(id)s;"""
 
-        result = connectToMySQL(DB).query_db(query, data)
+        result = connectToMySQL(cls.db_name).query_db(query, data)
         print("result of query:")
         print(result)
         result = result[0]
         print(result)
         workout = cls(result)
-
-
-
 
         workout.user = user.User(
             {
@@ -66,10 +56,9 @@ class Workout:
                 "last_name": result["last_name"],
                 "email": result ["email"],
                 "password": False,
+                "account_type": result["account_type"],
                 "created_at": result["created_at"],
                 "updated_at": result["updated_at"],
-
-
             }
         )
         return workout
@@ -79,14 +68,14 @@ class Workout:
 
         data={"id": workout_id}
         query = "DELETE from workouts WHERE id = %(id)s;"
-        connectToMySQL(DB).query_db(query, data)
+        connectToMySQL(cls.db_name).query_db(query, data)
 
         return workout_id
 
     @classmethod
     def update_workout(cls, workout_dict, session_id):
         # print(workout_dict["id"])
-        workout = cls.get_by_id(workout_dict["id"])
+        workout = cls.get_workout_by_id(workout_dict["id"])
         # if workout.user.id != session_id:
         #     flash("You must be the creator to update this routine ")
         #     return False
@@ -97,41 +86,48 @@ class Workout:
         query = """UPDATE workouts
                     SET experience = %(experience)s, goals = %(goals)s, days = %(days)s, routine= %(routine)s
                     WHERE id = %(id)s;"""
-        result = connectToMySQL(DB).query_db(query, workout_dict)
-        workout = cls.get_by_id(workout_dict["id"])
+        result = connectToMySQL(cls.db_name).query_db(query, workout_dict)
+        workout = cls.get_workout_by_id(workout_dict["id"])
 
         return workout
 
     @classmethod
     def get_all(cls):
-        query = """ SELECT
-                    workouts.id, workouts.created_at, workouts.updated_at, experience, goals, days, routine,
-                    users.id as user_id, first_name, last_name, email, users.created_at, users.updated_at
-                    FROM workouts
-                    JOIN users on users.id = workouts.user_id;"""
-        workout_data = connectToMySQL(DB).query_db(query)
+        query = """
+        SELECT * FROM workouts
+        JOIN users
+        ON workouts.user_id = users.id ;
+        """
+        results = connectToMySQL(cls.db_name).query_db(query)
+        print(results)
+        #if we dont find any workoutss return an empty list
+        if len(results)==0:
+            return []
+        else:
+            all_workouts_object = [] # this will hold Trails
+            for workouts_dictionary in results:
 
-        workouts = []
+                #create the workouts
+                workouts_obj = cls(workouts_dictionary)
+                # Grab the user object
+                user_dictionary = {
+                    "id": workouts_dictionary["users.id"],
+                    "first_name": workouts_dictionary["first_name"],
+                    "last_name": workouts_dictionary["last_name"],
+                    "email": workouts_dictionary["email"],
+                    "password": workouts_dictionary["password"],
+                    "account_type": workouts_dictionary["account_type"],
+                    "created_at": workouts_dictionary["users.created_at"],
+                    "updated_at": workouts_dictionary["users.updated_at"],
+                }
 
-        for workout in workout_data:
-
-            workout_obj = cls(workout)
-
-            workout_obj.user = user.User(
-            {
-                "id": workout["user_id"],
-                "first_name": workout["first_name"],
-                "last_name": workout["last_name"],
-                "email": workout["email"],
-                "password": False,
-                "created_at": workout["created_at"],
-                "updated_at": workout["updated_at"]
-
-            }
-        )
-            workouts.append(workout_obj)
-
-        return workouts
+                # create user object
+                user_obj = user.User(user_dictionary)
+                # Link this user to this workoutsi
+                workouts_obj.user = user_obj
+                #add this workouts to the list of all workouts objects
+                all_workouts_object.append(workouts_obj)
+            return all_workouts_object
 
     @staticmethod
     def is_valid(workout_dict):
@@ -145,6 +141,5 @@ class Workout:
         if len(workout_dict["routine"]) < 15:
             flash("Enter your routine with description to help others!")
             valid = False
-
 
         return valid
