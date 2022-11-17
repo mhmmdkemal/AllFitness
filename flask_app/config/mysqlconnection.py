240bb8fd451a25dc3b4dd90bc9887be52b1c9def
Joshua Rosado Olivencia <joshua06rosado@gmail.com>
2022-11-16 22:22:49 -0700

    CSS Final Updates

--- flask_app/config/mysqlconnection.py
+++ flask_app/config/mysqlconnection.py
@@ -7,7 +7,7 @@ class MySQLConnection:
         # change the user and password as needed
         connection = pymysql.connect(host='localhost',
         user='root',
-        password='root',
+        password='Abril0406!',
         db=db,
         charset='utf8mb4',
         cursorclass=pymysql.cursors.DictCursor,
